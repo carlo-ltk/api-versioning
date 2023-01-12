f2a37c3ce0bd0fc033c783cabd13031a190f708d
Carlo D'Ambrosio <carlo.dambrosio@lovetoknow.com>
2023-01-12 15:18:34 +0100

    feat: use ssm parameters store to keep record of available versions

--- forget.sh
+++ forget.sh
@@ -4,7 +4,5 @@ version=${1:-stable}
 
 echo forgetting \"$version\" ... "\n"
 npm run serverless remove -- --stage $version
-jq --arg version "$version" 'del(.versions[] | select (. == $version))'  infra/versions.auto.tfvars.json > tmp.$$.json && mv tmp.$$.json infra/versions.auto.tfvars.json
+aws ssm put-parameter --name fnd-api-s2-versions --value "$(aws ssm get-parameter --name fnd-api-s2-versions --query "Parameter.Value" --output text  | jq --arg version "$version" 'del(.[] | select (. == $version))')" --overwrite
 terraform -chdir=infra apply --auto-approve
-
-
