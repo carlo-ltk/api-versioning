f5437ceb8f071c920dfbff977d2975c3ba202869
Carlo D'Ambrosio <carlo.dambrosio@lovetoknow.com>
2023-01-16 19:30:54 +0100

    fix: do not show version in the response, only the stack

--- app/handler.js
+++ app/handler.js
@@ -2,17 +2,17 @@
 
 module.exports.ping = async (event) => {
 
-  const versions = JSON.parse(process.env.versions)
+  //const versions = JSON.parse(process.env.versions)
+  //console.log(versions)
   const stage = process.env.stage
-
-  const version = versions.filter( (item) => item.stage == stage).shift().tag
+  //const version = versions.filter( (item) => item.stage == stage).shift().tag
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: `Hey there, I'm the ping function and I executed successfully!\n
-        Version: ${version} (${process.env.version})`,
+        (${process.env.stage})`,
         // input: event,
       },
       null,
